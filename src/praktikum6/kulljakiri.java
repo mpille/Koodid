12ef38aacdda94a137799aa882d66ffe1505454c
Martin Erik Pille <mpille@klass2-11-l.intra.itcollege.ee>
2016-09-30 09:53:28 +0300

    parandus

--- src/praktikum6/kulljakiri.java
+++ src/praktikum6/kulljakiri.java
@@ -1,8 +1,8 @@
-package praktikum6;
-
-public class kulljakiri {
-	
-	public static int kasutajaSisestus(String kysimus, int min, int max){
-System.out.println("Kull või kiri?");
-}
-}
+//package praktikum6;
+//
+//public class kulljakiri {
+//	
+//	public static int kasutajaSisestus(String kysimus, int min, int max){
+//System.out.println("Kull või kiri?");
+//}
+//}
