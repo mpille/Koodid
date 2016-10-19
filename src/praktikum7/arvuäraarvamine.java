43f8bb82fa46a6319e87e58b11d2579f26d62d42
Martin Erik Pille <Martin Erik Pille>
2016-10-19 15:24:50 +0300

    parandus?

--- src/praktikum7/arvuäraarvamine.java
+++ src/praktikum7/arvuäraarvamine.java
@@ -7,18 +7,18 @@ public class arvu‰raarvamine {
 	public static void main(String[] args) {
 
 		int tundmatuarv = (int) (Math.random() * 100);
-		System.out.println("Arva √§ra arv 1 kuni 100");
+		System.out.println("Arva ‰ra arv 1 kuni 100");
 		while (true) {
 			// System.out.println(tundmatuarv);
 			int kasutajaarv = TextIO.getlnInt();
 			if (kasutajaarv == tundmatuarv) {
-				System.out.println("√ïige arv!");
+				System.out.println("’ige arv!");
 				break;
 			}
 			if (kasutajaarv < tundmatuarv)
 				System.out.println("Arv on suurem, proovi uuesti");
 			if (kasutajaarv > tundmatuarv)
-				System.out.println("Arv on v√§iksem, proovi uuesti");
+				System.out.println("Arv on v‰iksem, proovi uuesti");
 		}
 	}
 }
